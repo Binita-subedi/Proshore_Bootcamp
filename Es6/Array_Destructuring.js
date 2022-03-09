a5dc6af8ee54789eb139840c37d052b286a0190b
Binita-subedi <subedibinita94@gmail.com>
2022-03-09 18:40:52 +0545

    Description on Read.me

--- Es6/Array_Destructuring.js
+++ Es6/Array_Destructuring.js
@@ -1,3 +1,6 @@
+//The Destructing ssingment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables. 
+
+
 const myproglang = ['js', 'php', 'c', 'python', 'java'];
 
 //es5
