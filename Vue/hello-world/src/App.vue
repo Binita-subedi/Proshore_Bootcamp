6c280fee80dceb20a1cde610ed44a7d684f6b020
Binita-subedi <subedibinita94@gmail.com>
2022-03-17 19:21:25 +0545

    vue v-binding with class

--- Vue/hello-world/src/App.vue
+++ Vue/hello-world/src/App.vue
@@ -6,6 +6,29 @@
     <div v-html="hack"></div>
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disable="isDisabled">Bind</button>
+    <h2 class="underline">Underlined text</h2>
+    <h2 v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">MOVIES</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">
+      The movie has sold out
+    </h2>
+    <h2
+      v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']"
+    >
+      Array conditional movie
+    </h2>
+    <h2
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldout,
+        'sold-out': isSoldout,
+      }"
+    >
+      Object conditional movie
+    </h2>
   </div>
 </template>
 
@@ -21,6 +44,9 @@ export default {
       hack: `<a href="#" onclick="alert('You have been hacked !')">Win a Prize</a>`,
       headingId: "heading",
       isDisabled: false,
+      status: "sucess",
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 };
@@ -34,5 +60,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: rgb(185, 113, 113);
 }
 </style>
