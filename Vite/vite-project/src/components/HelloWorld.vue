03a10ca70e95846dfe7344a6ddcb3af9022b620d
Binita-subedi <subedibinita94@gmail.com>
2022-03-20 00:49:07 +0545

    Conditional Rendering

---
+++ Vite/vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2 v-if="num === 0">The number is zere</h2>
+    <h2 v-else-if="num < 0">The number is negative</h2>
+    <h2 v-else-if="num > 0">The number is positive</h2>
+    <h2 v-else>Not a number</h2>
+
+    <template v-if="display">
+      <h2>Binita</h2>
+      <h2>Codevolution</h2>
+      <h2>Vue</h2>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      num: "Hi",
+      display: true,
+    };
+  },
+};
+</script>
