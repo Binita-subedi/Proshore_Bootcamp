b8926a44c5c8b5bebeb2dae9ad2c148a12cbcbb0
Binita-subedi <subedibinita94@gmail.com>
2022-03-26 20:46:35 +0545

    List Rendering

--- Vite/vite-project/src/components/ListRendering.vue
+++ Vite/vite-project/src/components/ListRendering.vue
@@ -1,15 +1,44 @@
 <template>
   <div>
-    <h2 v-for="name in names" :key="name">{{ name }}</h2>
+    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+    <h2 v-for="name in fullNames" :key="name.first">
+      {{ name.first }} {{ name.last }}
+    </h2>
+    <div v-for="actor in actors" :key="actor.name">
+      <h2>{{ actor.name }}</h2>
+      <h2 v-for="movie in actor.movies" :key="movie">{{ movie }}</h2>
+    </div>
+    <h2 v-for="(value, key, index) in myInfo" :key="value">
+      {{ index }} {{ key }} {{ value }}
+    </h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ListRendering",
   data() {
     return {
       names: ["Bruse", "Clark", "Diana"],
+      fullNames: [
+        { first: "Bruce", last: "Wayne" },
+        { first: "Clark", last: "Kent" },
+        { first: "Princess", last: "Diana" },
+      ],
+      actors: [
+        {
+          name: "Christian Bale",
+          movies: ["Batman", "The  Pestige"],
+        },
+        {
+          name: "Di Caprio",
+          movies: ["Titanic", "Inception"],
+        },
+      ],
+      myInfo: {
+        name: "Binita",
+        channel: "Codevolution",
+        course: "Vue 3",
+      },
     };
   },
 };
