031279c1ad94a0d79ccb857f44cb14d1acb62b89
Binita-subedi <subedibinita94@gmail.com>
2022-04-18 15:45:27 +0545

    Vue-Route

--- Route/vue-route/src/App.vue
+++ Route/vue-route/src/App.vue
@@ -1,32 +1,67 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+<div>
+    <div class="nav">
+      <router-link to="/">Welcome</router-link> 
+      <router-link to="/home">Home</router-link> 
+      <router-link :to="{ name: 'About' }">About</router-link> 
+      <router-link :to="{ name: 'Jobs' }">Jobs</router-link> 
+      
+    </div>
+
+    <button @click="redirect">Redirect</button>
+    <button @click="back">Go Back</button>
+    <button @click="forward">Go Farward</button>
+
+  <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Welcome'})
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+    }
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
+.nav {
   padding: 30px;
 }
 
-nav a {
+.nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
--- Route/vue-route/src/router/index.js
+++ Route/vue-route/src/router/index.js
@@ -1,18 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import HomePage from '../views/HomePage.vue'
+import Jobs from '../views/Job/Jobs.vue'
+import JobDetails from '../views/Job/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'Welcome',
+    component: HomePage
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/about-us',
+    name: 'About',
     component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs',
+  },
+  // catchAll 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
+
 ]
 
 const router = createRouter( {
