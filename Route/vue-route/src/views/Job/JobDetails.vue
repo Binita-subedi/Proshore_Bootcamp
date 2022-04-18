41f51545936beac3f087b23f825b489c2a41c0ba
Binita-subedi <subedibinita94@gmail.com>
2022-04-18 17:48:51 +0545

    Fetching data using json.server

--- Route/vue-route/src/views/Job/JobDetails.vue
+++ Route/vue-route/src/views/Job/JobDetails.vue
@@ -1,14 +1,28 @@
  <template>
- <div>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+ <div v-if="job">
+   <h1>{{ job.title }}</h1>
+    <h1>The Job id is {{ id }}</h1>
+    <p>{{ job.details }}</p>
+ </div>
+ <div v-else>
+   <p>Loading job datails...</p>
  </div>
  </template>
 
  <script>
    export default {
      props: ['id'],
-    
+     data(){
+       return {
+         job: null
+       }
+     },
+    mounted(){
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    }
    }
  </script>
 
