d36c28dbf973d78067897103a342373da73aa187
Binita-subedi <subedibinita94@gmail.com>
2022-04-20 17:28:58 +0545

    ref vs reactive

--- blog/src/views/HomeView.vue
+++ blog/src/views/HomeView.vue
@@ -6,6 +6,9 @@
    <p>{{ ninjaOne.name}} - {{ ninjaOne.age}}</p>
    <button @click="updateNinjaOne">Update ninja one</button>
    <button @click="router.push('/reactivity')">Redirect</button>
+   <h2>Reactive</h2>
+   <p>{{ ninjaTwo.name}} - {{ ninjaTwo.age}}</p>
+   <button @click="updateNinjaTwo">Update ninja Two</button>
   </div>
 </template>
 
@@ -16,13 +19,17 @@ import { useRouter } from 'vue-router';
 
 
     const ninjaOne = ref({ name: 'mario', age: 30 })
+    const ninjaTwo = reactive({ name:'luigi', age: 35})
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40
     }
-     const router = useRouter()
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 50
+    }
+    const router = useRouter()
    
   
-
+ 
  </script>
 
 
