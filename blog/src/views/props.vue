82836492c8c81529a1d21f3066d770f494f67eed
Binita-subedi <subedibinita94@gmail.com>
2022-04-21 17:48:26 +0545

    Fetching data with composition

--- blog/src/views/props.vue
+++ blog/src/views/props.vue
@@ -1,7 +1,12 @@
 <template>
 <div class="home">
   <h1>Props</h1>
-  <Postlist v-if="showPosts" :posts="posts" />
+  <div v-if="error">{{ error }}</div>
+  <div v-if="posts.length">
+    <Postlist v-if="showPosts" :posts="posts" />
+  </div>
+  <div v-else>Loading....</div>
+  
   <button @click="showPosts = !showPosts">toggle Posts</button>
   <button @click="posts.pop()">delete a post</button>
 </div>
@@ -13,14 +18,28 @@ import Postlist from '../components/Postlist.vue'
 import { ref } from 'vue'
 
 export default {
-  name: 'props',
+  name: 'props', 
   components: { Postlist },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', id: 1},
-      { title: 'top 5 css tips', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s', id: 2},
-    ])
+    const posts = ref([])
+    const error = ref(null)
     const showPosts = ref(true)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+      
+    }
+    load()
 
     return { posts, showPosts }
   }
