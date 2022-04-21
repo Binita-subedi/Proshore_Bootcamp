43e0ba2ab9a5a1445d04404c76f430385f534752
Binita-subedi <subedibinita94@gmail.com>
2022-04-21 13:41:42 +0545

    Life cycle hook

--- blog/src/views/props.vue
+++ blog/src/views/props.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="home">
   <h1>Props</h1>
-  <Postlist :posts="posts" />
-
+  <Postlist v-if="showPosts" :posts="posts" />
+  <button @click="showPosts = !showPosts">toggle Posts</button>
+  <button @click="posts.pop()">delete a post</button>
 </div>
 </template>
 
@@ -12,14 +13,16 @@ import Postlist from '../components/Postlist.vue'
 import { ref } from 'vue'
 
 export default {
+  name: 'props',
   components: { Postlist },
-  name: 'Home',
   setup() {
     const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', id: 1},
-      { title: 'top 5 css tips', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', id: 2},
+      { title: 'welcome to the blog', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', id: 1},
+      { title: 'top 5 css tips', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s', id: 2},
     ])
-    return { posts }
+    const showPosts = ref(true)
+
+    return { posts, showPosts }
   }
 } 
 </script>
