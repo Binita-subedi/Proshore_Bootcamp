cbf2f6fff3d0a54c095b2c4345753ff5f0beee00
Binita-subedi <subedibinita94@gmail.com>
2022-05-03 12:56:17 +0545

    add transform/errorhandling/cancel

--- Axios/main.js
+++ Axios/main.js
@@ -1,8 +1,11 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 // GET REQUEST
 function getTodos() {
   axios({
     method: 'get',
-    url: 'https://jsonplaceholder.typicode.com/todos',
+    url: 'https://jsonplaceholder.typicode.com/todos',timeout: 5000,
     params: {
       _limit: 5
     }
@@ -94,17 +97,64 @@ axios.post('https://jsonplaceholder.typicode.com/todos', {
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method: 'post',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults. transformResponse.concat(data => {
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  };
+  axios(options).then(res => showOutput(res));
 }
  
 // ERROR HANDLING
  function errorHandling() {
-  console.log('Error Handling');
+  axios
+  .get('https://jsonplaceholder.typicode.com/todoss')
+  .then(res => showOutput(res))
+  .catch(err => {
+    if (error.response) {
+      //server responded with a status other than 200 range
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+
+      if(err.response.status === 404) {
+        alert('Error: Page Not Found');
+      }
+    }else if (err.request) {
+      // Request was made but no response
+      console.error(err.request);
+    } else {
+      console.error(err.message);
+    }
+  });
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+function cancelToken() {
+  const source = axios.CancelToken.source();
+
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      cancelToken: source.token
+    })
+    .then(res => showOutput(res))
+    .catch(thrown => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message);
+      }
+    });
+
+  if (true) {
+    source.cancel('Request canceled!');
+  }
+}
 }
 
 // INTERCEPTING REQUESTS & RESPONSES  
@@ -124,12 +174,18 @@ axios.interceptors.request.use(
 );
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+  // Other custom settings
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+axiosInstance.get('/comments').then(res => showOutput(res));
+
 
 // Show output in browser
 function showOutput(res) {
   document.getElementById('res').innerHTML = `
   <div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
+    <h5>Status: ${ res.status}</h5>
   </div>
   <div class="card mt-3">
     <div class="card-header">
